677c6186ac8afa18c589293192b6fca2181f896b
Nelman25 <jonelvillaver735@gmail.com>
2024-04-03 04:22:05 +0800

    Learning java swing buttons

--- JavaProgrammingPractice/Main.java
+++ JavaProgrammingPractice/Main.java
@@ -1,54 +1,5 @@
-import java.awt.Color;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
 public class Main {
     public static void main(String[] args) {
-
-        ImageIcon image = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\like (1).png");
-
-        JLabel label = new JLabel();
-        label.setText("Hi!");
-        label.setIcon(image);
-        // label.setHorizontalAlignment(JLabel.CENTER);
-        // label.setVerticalAlignment(JLabel.CENTER);
-        label.setBounds(0, 0, 75,75);
-        
-        JPanel redPanel = new JPanel();
-        redPanel.setBackground(Color.red);
-        redPanel.setBounds(0,0,250,250);
-        // redPanel.setLayout(new BorderLayout());
-        redPanel.setLayout(null);
-        
-        JPanel bluePanel = new JPanel();
-        bluePanel.setBackground(Color.blue);
-        bluePanel.setBounds(250,0,250,250);
-        // bluePanel.setLayout(new BorderLayout());
-        bluePanel.setLayout(null);
-
-        JPanel yellowPanel = new JPanel();
-        yellowPanel.setBackground(Color.yellow);
-        yellowPanel.setBounds(0,250,250,250);   
-        // yellowPanel.setLayout(new BorderLayout());
-        yellowPanel.setLayout(null);
-
-        JPanel orangePanel = new JPanel();
-        orangePanel.setBackground(Color.orange);
-        orangePanel.setBounds(250,250,250,250);
-        // orangePanel.setLayout(new BorderLayout()); 
-        orangePanel.setLayout(null);
-        
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(750,750);
-        frame.setVisible(true);
-        frame.setLayout(null);
-        orangePanel.add(label);
-        frame.add(redPanel);
-        frame.add(bluePanel);
-        frame.add(yellowPanel);
-        frame.add(orangePanel);
+        new MyFrame();
     }
 }
--- JavaProgrammingPractice/MyFrame.java
+++ JavaProgrammingPractice/MyFrame.java
@@ -1,12 +1,50 @@
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class MyFrame extends JFrame {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener{
+    JButton button;
+    JLabel label;
+
     MyFrame() {
 
-        this.setVisible(true);
-        this.setSize(500,500);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("GUI Testing");
+        ImageIcon image = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\like (1) (1).png");
+        ImageIcon image2 = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\wow emoji (1).png");
+
+        label = new JLabel();
+        label.setIcon(image2);
+        label.setVisible(false);
+        label.setBounds(200,150,250,150);
         
+        button = new JButton();
+        button.setBounds(200,100,100,50);
+        button.addActionListener(this);
+        button.setText("Like");
+        button.setFocusable(false);
+        button.setIcon(image);
+        button.setFont(new Font("Arial",Font.BOLD,14));
+        button.setBackground(new Color(173,216,230));
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setVisible(true);
+        this.setLayout(null);
+        this.add(button);
+        this.add(label);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==button) {
+            System.out.println("like");
+            label.setVisible(true);
+        }
     }
 }
