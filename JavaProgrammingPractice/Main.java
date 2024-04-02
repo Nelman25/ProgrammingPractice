6c671ceaf671ac39fa194cbd5684971f42ba2160
Nelman25 <jonelvillaver735@gmail.com>
2024-04-02 23:15:22 +0800

    Learning java GUI panel

--- JavaProgrammingPractice/Main.java
+++ JavaProgrammingPractice/Main.java
@@ -1,43 +1,54 @@
 import java.awt.Color;
-import java.awt.Font;
-
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.border.Border;
+import javax.swing.JPanel;
 
 public class Main {
     public static void main(String[] args) {
 
-        ImageIcon image = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\beachoten (1).png");
-        Border border = BorderFactory.createLineBorder(Color.green,2);
+        ImageIcon image = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\like (1).png");
 
         JLabel label = new JLabel();
-        label.setText("Bitch, nigahi na ang akong oten?");
+        label.setText("Hi!");
         label.setIcon(image);
-        label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setVerticalTextPosition(JLabel.TOP);
-        label.setForeground(new Color(0x00FF00));
-        label.setFont(new Font ("Arial",Font.ITALIC,20));
-        label.setIconTextGap(10);
-        label.setBackground(Color.black);
-        label.setOpaque(true);
-        label.setBorder(border);
-        label.setVerticalAlignment(JLabel.CENTER);
-        label.setHorizontalAlignment(JLabel.CENTER);
-        // label.setBounds(100,100,250,250);
+        // label.setHorizontalAlignment(JLabel.CENTER);
+        // label.setVerticalAlignment(JLabel.CENTER);
+        label.setBounds(0, 0, 75,75);
         
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0,0,250,250);
+        // redPanel.setLayout(new BorderLayout());
+        redPanel.setLayout(null);
+        
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(250,0,250,250);
+        // bluePanel.setLayout(new BorderLayout());
+        bluePanel.setLayout(null);
 
+        JPanel yellowPanel = new JPanel();
+        yellowPanel.setBackground(Color.yellow);
+        yellowPanel.setBounds(0,250,250,250);   
+        // yellowPanel.setLayout(new BorderLayout());
+        yellowPanel.setLayout(null);
+
+        JPanel orangePanel = new JPanel();
+        orangePanel.setBackground(Color.orange);
+        orangePanel.setBounds(250,250,250,250);
+        // orangePanel.setLayout(new BorderLayout()); 
+        orangePanel.setLayout(null);
+        
         JFrame frame = new JFrame();
-        frame.setVisible(true);
-        frame.setSize(500,500);
-        frame.setTitle("Java Swing GUI Practice");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(label);
-        frame.getContentPane().setBackground(Color.black);
-        // frame.setLayout(null);
-        frame.pack();
-
+        frame.setSize(750,750);
+        frame.setVisible(true);
+        frame.setLayout(null);
+        orangePanel.add(label);
+        frame.add(redPanel);
+        frame.add(bluePanel);
+        frame.add(yellowPanel);
+        frame.add(orangePanel);
     }
 }
