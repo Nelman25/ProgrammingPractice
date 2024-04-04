0777f7e56eb04a118332503c5aa6db8cf1718f76
Nelman25 <jonelvillaver735@gmail.com>
2024-04-04 09:19:42 +0800

    Learning Java Swing border layout

--- JavaProgrammingPractice/MyFrame.java
+++ JavaProgrammingPractice/MyFrame.java
@@ -1,50 +1,74 @@
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Dimension;
 
-public class MyFrame extends JFrame implements ActionListener{
-    JButton button;
-    JLabel label;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class MyFrame extends JFrame{
     MyFrame() {
-
-        ImageIcon image = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\like (1) (1).png");
-        ImageIcon image2 = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\wow emoji (1).png");
-
-        label = new JLabel();
-        label.setIcon(image2);
-        label.setVisible(false);
-        label.setBounds(200,150,250,150);
-        
-        button = new JButton();
-        button.setBounds(200,100,100,50);
-        button.addActionListener(this);
-        button.setText("Like");
-        button.setFocusable(false);
-        button.setIcon(image);
-        button.setFont(new Font("Arial",Font.BOLD,14));
-        button.setBackground(new Color(173,216,230));
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
         this.setVisible(true);
-        this.setLayout(null);
-        this.add(button);
-        this.add(label);
+        this.setLayout(new BorderLayout(10,10));
+        this.setTitle("Java Swing BorderLayout Practice");
 
-    }
+        // ------------------------ MAIN PANEL ----------------------------------
+        
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==button) {
-            System.out.println("like");
-            label.setVisible(true);
-        }
+        panel1.setBackground(Color.red);
+        panel2.setBackground(Color.green);
+        panel3.setBackground(Color.yellow);
+        panel4.setBackground(Color.MAGENTA);
+        panel5.setBackground(Color.blue);
+
+        panel1.setPreferredSize(new Dimension(100,100));
+        panel2.setPreferredSize(new Dimension(100,100));
+        panel3.setPreferredSize(new Dimension(100,100));
+        panel4.setPreferredSize(new Dimension(100,100));
+        panel5.setPreferredSize(new Dimension(100,100));
+
+        this.add(panel1,BorderLayout.NORTH);
+        this.add(panel2,BorderLayout.WEST);
+        this.add(panel3,BorderLayout.EAST);
+        this.add(panel4,BorderLayout.SOUTH);
+        this.add(panel5,BorderLayout.CENTER);
+
+        // ------------------------ MAIN PANEL ----------------------------------
+
+        // ------------------------------SUB PANELS ---------------------------------
+
+        JPanel panel6 = new JPanel();
+        JPanel panel7 = new JPanel();
+        JPanel panel8 = new JPanel();
+        JPanel panel9 = new JPanel();
+        JPanel panel10 = new JPanel();
+
+        panel6.setBackground(Color.BLACK);
+        panel7.setBackground(Color.CYAN);
+        panel8.setBackground(Color.DARK_GRAY);
+        panel9.setBackground(Color.ORANGE);
+        panel10.setBackground(Color.PINK);
+
+        panel6.setPreferredSize(new Dimension(50,50));
+        panel7.setPreferredSize(new Dimension(50,50));
+        panel8.setPreferredSize(new Dimension(50,50));
+        panel9.setPreferredSize(new Dimension(50,50));
+        panel10.setPreferredSize(new Dimension(50,50));
+
+        panel5.setLayout(new BorderLayout(10,10));
+        panel5.add(panel6, BorderLayout.NORTH);
+        panel5.add(panel7, BorderLayout.WEST);
+        panel5.add(panel8, BorderLayout.EAST);
+        panel5.add(panel9, BorderLayout.SOUTH);
+        panel5.add(panel10, BorderLayout.CENTER);
+
+        // ------------------------------SUB PANELS ---------------------------------
     }
 }
