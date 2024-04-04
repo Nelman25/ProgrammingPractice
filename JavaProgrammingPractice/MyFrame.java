f303429335c74310cbb8e7d52a686f9252df1d25
Nelman25 <jonelvillaver735@gmail.com>
2024-04-04 09:42:52 +0800

    Java Swing Flow Layout practice

--- JavaProgrammingPractice/MyFrame.java
+++ JavaProgrammingPractice/MyFrame.java
@@ -1,74 +1,35 @@
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class MyFrame extends JFrame{
+public class MyFrame extends JFrame {
     MyFrame() {
+
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(150,250));
+        panel.setLayout(new FlowLayout());
+        panel.setBackground(Color.lightGray);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
+        this.setTitle("Java Swing Flow Layout practice");
+        this.setLayout(new FlowLayout());
+        this.add(panel);
+
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("9"));
+
         this.setVisible(true);
-        this.setLayout(new BorderLayout(10,10));
-        this.setTitle("Java Swing BorderLayout Practice");
-
-        // ------------------------ MAIN PANEL ----------------------------------
-        
-        JPanel panel1 = new JPanel();
-        JPanel panel2 = new JPanel();
-        JPanel panel3 = new JPanel();
-        JPanel panel4 = new JPanel();
-        JPanel panel5 = new JPanel();
-
-        panel1.setBackground(Color.red);
-        panel2.setBackground(Color.green);
-        panel3.setBackground(Color.yellow);
-        panel4.setBackground(Color.MAGENTA);
-        panel5.setBackground(Color.blue);
-
-        panel1.setPreferredSize(new Dimension(100,100));
-        panel2.setPreferredSize(new Dimension(100,100));
-        panel3.setPreferredSize(new Dimension(100,100));
-        panel4.setPreferredSize(new Dimension(100,100));
-        panel5.setPreferredSize(new Dimension(100,100));
-
-        this.add(panel1,BorderLayout.NORTH);
-        this.add(panel2,BorderLayout.WEST);
-        this.add(panel3,BorderLayout.EAST);
-        this.add(panel4,BorderLayout.SOUTH);
-        this.add(panel5,BorderLayout.CENTER);
-
-        // ------------------------ MAIN PANEL ----------------------------------
-
-        // ------------------------------SUB PANELS ---------------------------------
-
-        JPanel panel6 = new JPanel();
-        JPanel panel7 = new JPanel();
-        JPanel panel8 = new JPanel();
-        JPanel panel9 = new JPanel();
-        JPanel panel10 = new JPanel();
-
-        panel6.setBackground(Color.BLACK);
-        panel7.setBackground(Color.CYAN);
-        panel8.setBackground(Color.DARK_GRAY);
-        panel9.setBackground(Color.ORANGE);
-        panel10.setBackground(Color.PINK);
-
-        panel6.setPreferredSize(new Dimension(50,50));
-        panel7.setPreferredSize(new Dimension(50,50));
-        panel8.setPreferredSize(new Dimension(50,50));
-        panel9.setPreferredSize(new Dimension(50,50));
-        panel10.setPreferredSize(new Dimension(50,50));
-
-        panel5.setLayout(new BorderLayout(10,10));
-        panel5.add(panel6, BorderLayout.NORTH);
-        panel5.add(panel7, BorderLayout.WEST);
-        panel5.add(panel8, BorderLayout.EAST);
-        panel5.add(panel9, BorderLayout.SOUTH);
-        panel5.add(panel10, BorderLayout.CENTER);
-
-        // ------------------------------SUB PANELS ---------------------------------
     }
 }
