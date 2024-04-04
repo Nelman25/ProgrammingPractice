ef5fbac4a864362d290d50f06f813f33c802ce93
Nelman25 <jonelvillaver735@gmail.com>
2024-04-04 16:26:13 +0800

    Java Swing grid layout practice

--- JavaProgrammingPractice/MyFrame.java
+++ JavaProgrammingPractice/MyFrame.java
@@ -1,35 +1,25 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class MyFrame extends JFrame {
     MyFrame() {
-
-        JPanel panel = new JPanel();
-        panel.setPreferredSize(new Dimension(150,250));
-        panel.setLayout(new FlowLayout());
-        panel.setBackground(Color.lightGray);
-
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
-        this.setTitle("Java Swing Flow Layout practice");
-        this.setLayout(new FlowLayout());
-        this.add(panel);
+        this.setLayout(new GridLayout(3,3,10,10));
 
-        panel.add(new JButton("1"));
-        panel.add(new JButton("2"));
-        panel.add(new JButton("3"));
-        panel.add(new JButton("4"));
-        panel.add(new JButton("5"));
-        panel.add(new JButton("6"));
-        panel.add(new JButton("7"));
-        panel.add(new JButton("8"));
-        panel.add(new JButton("9"));
+        this.add(new JButton("1"));
+        this.add(new JButton("2"));
+        this.add(new JButton("3"));
+        this.add(new JButton("4"));
+        this.add(new JButton("5"));
+        this.add(new JButton("6"));
+        this.add(new JButton("7"));
+        this.add(new JButton("8"));
+        this.add(new JButton("9"));
 
         this.setVisible(true);
+
     }
 }
