23f7167d2eb72244d48b7fb89a8b6dc9f54c4c74
Nelman25 <jonelvillaver735@gmail.com>
2024-04-06 19:45:33 +0800

    Java Swing File chooser practice

--- JavaProgrammingPractice/MyFrame.java
+++ JavaProgrammingPractice/MyFrame.java
@@ -2,76 +2,39 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+import java.io.File;
 
 public class MyFrame extends JFrame implements ActionListener {
 
-    JMenuBar menubar;
-    JMenuItem loadItem;
-    JMenuItem exitItem;
-    JMenuItem saveItem;
-    ImageIcon loadIcon;
-    ImageIcon exitIcon;
-    ImageIcon saveIcon;
-    
+    JButton button;
+
     MyFrame() {
-        this.setTitle("Java Swing Menu bar practice");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(new FlowLayout());
         this.setSize(500,500);
+        this.setLayout(new FlowLayout());
 
-        loadIcon = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\file (1).png");
-        exitIcon = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\door (1).jpg");
-        saveIcon = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\save (1).png");
+        button = new JButton("Choose file");
+        button.addActionListener(this);
 
-        menubar = new JMenuBar();
-
-        JMenu fileMenu = new JMenu("File");
-        JMenu editMenu = new JMenu("Edit");
-        JMenu helpMenu = new JMenu("Help");
-
-        loadItem = new JMenuItem("Load");
-        saveItem = new JMenuItem("Save");
-        exitItem = new JMenuItem("Exit");
-
-        fileMenu.add(loadItem);
-        fileMenu.add(saveItem);
-        fileMenu.add(exitItem);
-
-        loadItem.setIcon(loadIcon);
-        saveItem.setIcon(saveIcon);
-        exitItem.setIcon(exitIcon);
-
-        loadItem.addActionListener(this);
-        saveItem.addActionListener(this);
-        exitItem.addActionListener(this);
-
-        fileMenu.setMnemonic(KeyEvent.VK_F);
-        editMenu.setMnemonic(KeyEvent.VK_E);
-        helpMenu.setMnemonic(KeyEvent.VK_H);
-        loadItem.setMnemonic(KeyEvent.VK_L);
-        saveItem.setMnemonic(KeyEvent.VK_S);
-        exitItem.setMnemonic(KeyEvent.VK_E);
-
-        menubar.add(fileMenu);
-        menubar.add(editMenu);
-        menubar.add(helpMenu);
-
-        this.setJMenuBar(menubar);
+        this.add(button);
+        this.pack();
         this.setVisible(true);
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==loadItem) {
-            System.out.println("You loaded an item");
-        }
-        else if(e.getSource()==saveItem) {
-            System.out.println("You saved an item");
-        }
-        else if(e.getSource()==exitItem) {
-            System.out.println("You exited the program");
-            System.exit(0);
+        if(e.getSource()==button) {
+            JFileChooser chooser = new JFileChooser();
+            chooser.setCurrentDirectory(new File("C:\\Users\\Jonel Villaver\\Desktop"));
+
+            //int response = chooser.showOpenDialog(null);      // this will select file to open
+            int response = chooser.showSaveDialog(null); // this will select a file to save (it doesn't create the file yet)
+
+            if(response == JFileChooser.APPROVE_OPTION) {
+                File file = new File(chooser.getSelectedFile().getAbsolutePath());
+                System.out.println(file); // prints the absolute path of selected file
+            }
         }
     }
 }
