ee61b3fe2d6254081f7d6c23faf7344378abd603
Nelman25 <jonelvillaver735@gmail.com>
2024-04-07 03:36:58 +0800

    Java Swing Key Listener practice (Fake Pacman Game)

--- JavaProgrammingPractice/MyFrame.java
+++ JavaProgrammingPractice/MyFrame.java
@@ -1,41 +1,70 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class MyFrame extends JFrame implements ActionListener {
+public class MyFrame extends JFrame implements KeyListener {
 
-    JButton button;
-    JLabel label;
+    JLabel background;
+    JLabel pacman;
 
     MyFrame() {
+
+        ImageIcon image = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\PackmanBackground.jpg");
+        ImageIcon image2 = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\pacman (1).png");
+
+        this.setTitle("Fake Pacman Game");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500, 500);
-        this.setLayout(new FlowLayout());
+        this.setSize(600,600);
+        this.setLayout(null);
 
-        button = new JButton("Choose a color");
-        button.addActionListener(this);
+        background = new JLabel();
+        background.setBounds(0,0,600,600);
+        background.setIcon(image);
+        background.setOpaque(true);
 
-        label = new JLabel("Choose a color");
-        label.setBackground(Color.white);
-        label.setOpaque(true);
-        label.setFont(new Font("MV Boli", Font.PLAIN, 100));
+        pacman = new JLabel();
+        pacman.setBounds(285,290,35,35);
+        pacman.setIcon(image2);
+        pacman.setOpaque(true);
 
-        this.add(button);
-        this.add(label);
-        this.pack();
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0,0,600,600);
+        layeredPane.add(background,Integer.valueOf(0));
+        layeredPane.add(pacman,Integer.valueOf(1));
+
+        this.addKeyListener(this);
+        this.add(layeredPane);
         this.setVisible(true);
+
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == button) {
-            JColorChooser colorChooser = new JColorChooser();
-
-            Color color = colorChooser.showDialog(null, "Choose a color", Color.white);
-
-//            label.setBackground(color);
-            label.setForeground(color);
+    public void keyTyped(KeyEvent e) {
+        switch(e.getKeyChar()) {
+            case 'a':
+                pacman.setLocation(pacman.getX()-3, pacman.getY());
+                break;
+            case 'd':
+                pacman.setLocation(pacman.getX()+3, pacman.getY());
+                break;
+            case 'w':
+                pacman.setLocation(pacman.getX(), pacman.getY()-3);
+                break;
+            case 's':
+                pacman.setLocation(pacman.getX(), pacman.getY()+3);
+                break;
         }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+//        System.out.println(e.getKeyChar());
+//        System.out.println(e.getKeyCode());
     }
 }
