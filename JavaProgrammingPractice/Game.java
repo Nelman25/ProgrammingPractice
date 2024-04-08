43bba515816139f694467664c94e4848030cfab9
Nelman25 <jonelvillaver735@gmail.com>
2024-04-09 01:27:18 +0800

    Java Swing Key Binding practice

---
+++ JavaProgrammingPractice/Game.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Game{
+
+    JFrame frame;
+    JLabel label;
+    Action upAction;
+    Action downAction;
+    Action leftAction;
+    Action rightAction;
+
+    Game() {
+        frame = new JFrame();
+        frame.setSize(420,420);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+
+        label = new JLabel();
+        label.setBounds(100,100,100,100);
+        label.setBackground(Color.red);
+        label.setOpaque(true);
+
+        upAction = new upAction();
+        downAction = new downAction();
+        leftAction = new leftAction();
+        rightAction = new rightAction();
+
+        label.getInputMap().put(KeyStroke.getKeyStroke('w'),"upAction");
+        label.getActionMap().put("upAction", upAction);
+        label.getInputMap().put(KeyStroke.getKeyStroke('s'),"downAction");
+        label.getActionMap().put("downAction", downAction);
+        label.getInputMap().put(KeyStroke.getKeyStroke('a'),"leftAction");
+        label.getActionMap().put("leftAction", leftAction);
+        label.getInputMap().put(KeyStroke.getKeyStroke('d'),"rightAction");
+        label.getActionMap().put("rightAction", rightAction);
+
+        frame.add(label);
+        frame.setVisible(true);
+    }
+    public class upAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            label.setLocation(label.getX(), label.getY()-10);
+        }
+    }
+    public class downAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            label.setLocation(label.getX(), label.getY()+10);
+        }
+    }
+    public class leftAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            label.setLocation(label.getX()-10, label.getY());
+        }
+    }
+    public class rightAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            label.setLocation(label.getX()+10, label.getY());
+        }
+    }
+}
--- JavaProgrammingPractice/MyFrame.java
+++ JavaProgrammingPractice/MyFrame.java
@@ -1,16 +1,61 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class MyFrame extends JFrame {
+public class MyFrame implements MouseListener {
 
-    DragPanel dragPanel = new DragPanel();
+    JFrame frame;
+    JLabel label;
+    ImageIcon normal;
+    ImageIcon hovered;
+    ImageIcon clicked;
+    ImageIcon pressed;
 
     MyFrame() {
 
-        this.add(dragPanel);
-        this.setTitle("Drag and Drop Practice");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(600, 600);
-        this.setVisible(true);
+        normal = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\normal.png");
+        hovered = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\hovered.png");
+        clicked = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\clicked.png");
+        pressed = new ImageIcon("C:\\Users\\Jonel Villaver\\Downloads\\pressed.png");
 
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(624,624);
+        frame.setLayout(null);
+
+        label = new JLabel();
+        label.setBounds(0,0,624,624);
+        label.setIcon(normal);
+        label.addMouseListener(this);
+
+        frame.add(label);
+        frame.setVisible(true);
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        label.setIcon(clicked);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        label.setIcon(pressed);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        label.setIcon(pressed);
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        label.setIcon(hovered);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        label.setIcon(normal);
     }
 }
