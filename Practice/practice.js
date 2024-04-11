de59b1b53d57f358e74d06d980f709adf0570bee
Nelman25 <jonelvillaver735@gmail.com>
2024-04-11 14:10:00 +0800

    Constructors in JavaScript update (function in constructor)

--- Practice/practice.js
+++ Practice/practice.js
@@ -2,7 +2,8 @@ function Car (make,model,year,color) {
     this.make = make,
     this.model = model,
     this.year = year,
-    this.color = color
+    this.color = color,
+    this.drive = function(){console.log(`You drive the ${this.model}`)}
 }
 
 const car1 = new Car("Tesla","model3",2024,"yellow");
@@ -10,9 +11,11 @@ console.log(car1.make);
 console.log(car1.model);
 console.log(car1.year);
 console.log(car1.color);
+car1.drive();
 
 const car2 = new Car("Chevrolet","mustang",2025,"lightblue");
 console.log(car2.make);
 console.log(car2.model);
 console.log(car2.year);
 console.log(car2.color);
+car2.drive();
