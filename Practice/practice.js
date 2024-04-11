957e542ff7f126b411dd372b3bd70258f30346e8
Nelman25 <jonelvillaver735@gmail.com>
2024-04-11 13:02:51 +0800

    JavaScript Objects practice

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,8 +1,28 @@
-const numbers = [1,2,3,4,5,6,7,8];
-const squared = numbers.map((element) => Math.pow(element,2));
-const cubed = numbers.map((element) => Math.pow(element,3));
-const even = numbers.filter((element) => element % 2 ===0);
-const odd = numbers.filter((element) => element % 2 !==0);
-const total = numbers.reduce((accumulator,element) => accumulator+element);
+// Object JavaScript practice
 
-console.log(total);
+const person1 = {
+    FirstName: "Dharilyn",
+    LastName: "Taladro",
+    age: 19,
+    isEmployed: false,
+    SayHello: () => console.log("Hello!")
+}
+const person2 = {
+    FirstName: "Jonel", 
+    LastName: "Villaver",
+    age: 19,
+    isEmployed: false,
+    SayHello: () => console.log("HELLO!")
+}
+
+console.log(person1.FirstName);
+console.log(person1.LastName);
+console.log(person1.age);
+console.log(person1.isEmployed);
+person1.SayHello();
+
+console.log(person2.FirstName);
+console.log(person2.LastName);
+console.log(person2.age);
+console.log(person2.isEmployed);
+person2.SayHello();
