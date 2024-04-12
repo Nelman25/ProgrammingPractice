2910a2618206bc5344ba3c19ff106f344d4378aa
Nelman25 <jonelvillaver735@gmail.com>
2024-04-13 06:59:16 +0800

    JavaScript Classes

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,21 +1,26 @@
-function Car (make,model,year,color) {
-    this.make = make,
-    this.model = model,
-    this.year = year,
-    this.color = color,
-    this.drive = function(){console.log(`You drive the ${this.model}`)}
+class Products {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    displayProduct() {
+        console.log(`Product: ${this.name}`);
+        console.log(`Price: $${this.price}`);
+    }
+    calculateTotal(salesTax) {
+        return this.price + (this.price * salesTax);
+    }
 }
 
-const car1 = new Car("Tesla","model3",2024,"yellow");
-console.log(car1.make);
-console.log(car1.model);
-console.log(car1.year);
-console.log(car1.color);
-car1.drive();
+const salesTax = 0.1;
+const Tshirt = new Products("T-shirt",19.99);
+const Bag = new Products("Bag",65.99);
+const TshirtTotal = Tshirt.calculateTotal(salesTax);
+const BagTotal = Bag.calculateTotal(salesTax);
 
-const car2 = new Car("Chevrolet","mustang",2025,"lightblue");
-console.log(car2.make);
-console.log(car2.model);
-console.log(car2.year);
-console.log(car2.color);
-car2.drive();
+Tshirt.displayProduct();
+console.log(`Total price (with tax): $${TshirtTotal.toFixed(2)}`);
+console.log();
+
+Bag.displayProduct();
+console.log(`Total price (with tax): $${BagTotal.toFixed(2)}`);
