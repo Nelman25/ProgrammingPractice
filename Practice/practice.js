d87b9680097199ea8cf72b309fe8060efadbb015
Nelman25 <jonelvillaver735@gmail.com>
2024-04-13 08:27:09 +0800

    super keyword in JavaScript

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,46 +1,68 @@
-// inheritance = it allows a new class to inherit properties and methods 
-//               from an existing class (parent class). User keyword "extends".
+// super = keyword used in classes to call the constructor or acces the properties 
+//         and methods of a parent (also known as superclass) 
+//         this = this object
+//         super = the parent
 
 class Animal {
-    alive = true;
-
-    eat() {
-        console.log(`This ${this.name} is eating`);
+    constructor(name,age) {
+        this.name = name;
+        this.age = age; 
     }
-    sleep() {
-        console.log(`This ${this.name} is sleeping`);
+    move(speed) {
+        console.log(`This ${this.name} moves at a speed of ${speed}mph`)
     }
 }
-
 class Rabbit extends Animal {
-    name = "Rabbit";
-
-    static run(){
-        console.log(`This ${this.name} is running`);
+    constructor(name,age,runSpeed) {
+        super(name,age);
+        this.runSpeed = runSpeed;
+    }
+    run() {
+        console.log(`This ${this.name} can run`);
+        super.move(this.runSpeed);
     }
 }
 
 class Fish extends Animal {
-    name = "Fish";
-
-    static swim() {
-        console.log(`This ${this.name} is swimming`);
+    constructor(name,age,swimSpeed) {
+        super(name,age);
+        this.swimSpeed = swimSpeed;
+    }
+    swim() {
+        console.log(`This ${this.name} can swim`);
+        super.move(this.swimSpeed);
     }
 }
 
 class Hawk extends Animal {
-    name = "Hawk";
-
-    static fly() {
-        console.log(`This ${this.name} is flying`);
+    constructor(name,age,flySpeed) {
+        super(name,age);
+        this.flySpeed = flySpeed;
+    }
+    fly() {
+        console.log(`This ${this.name} can fly`);
+        super.move(this.flySpeed);
     }
 }
 
-const rabbit = new Rabbit();
-const fish = new Fish();
-const hawk = new Hawk();
+const rabbit = new Rabbit("rabbit",3,15);
+const fish = new Fish("fish",5,20);
+const hawk = new Hawk("hawk",10,200);
 
-console.log(rabbit.alive);
-rabbit.eat();
-rabbit.sleep();
-console.log(Rabbit.run()); // using static keyword 
+console.log(rabbit.name);
+console.log(rabbit.age);
+console.log(rabbit.runSpeed);
+rabbit.run();
+
+console.log(fish.name);
+console.log(fish.age);
+console.log(fish.swimSpeed);
+fish.swim();
+
+console.log(hawk.name);
+console.log(hawk.age);
+console.log(hawk.flySpeed);
+hawk.fly();
+
+
+
